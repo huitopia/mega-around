509568ef44d09dfa3466b8dfb5d9b6fd73fe43d3
huitopia <huitopia27@gmail.com>
2024-07-17 20:39:07 +0900

    Spring : reaParam 및 service 추가

--- backend/src/main/java/com/backend/controller/product/ProductController.java
+++ backend/src/main/java/com/backend/controller/product/ProductController.java
@@ -1,23 +1,26 @@
 package com.backend.controller.product;
 
 import com.backend.domain.product.Product;
+import com.backend.service.product.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Description;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/products")
 public class ProductController {
 
+    private final ProductService service;
+
     @PostMapping
     @Description("상품 업로드")
     // TODO : admin 권한만 등록 가능하게
-    public void createProduct(@RequestBody Product product) {
-        // Product(id=1, title=null, mainCategory=null, subCategory=null, content=null, price=null, options=null, createdAt=null)
-        System.out.println("product = " + product);
+    public void createProduct(@RequestBody Product product,
+                              @RequestParam(value = "file", required = false) MultipartFile file)
+            throws Exception {
+        // TODO : validate method 생성
+        service.insertProduct(product, file);
     }
 }
