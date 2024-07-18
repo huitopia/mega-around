445ef96c1fec05bf56156afbc4c849209761630d
huitopia <huitopia27@gmail.com>
2024-07-18 15:23:43 +0900

    react : 상품 리스트 생성

---
+++ frontend/src/product/ProductList.jsx
@@ -0,0 +1,11 @@
+import { Box, Heading } from "@chakra-ui/react";
+
+export function ProductList() {
+  return (
+    <Box maxWidth="1000px" mx={"auto"}>
+      <Box>
+        <Heading>List</Heading>
+      </Box>
+    </Box>
+  );
+}
