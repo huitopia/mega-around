dba4a3848a9719754d008dcdc8fb65451e17cc42
huitopia <huitopia27@gmail.com>
2024-07-19 04:03:22 +0900

    react : ProductDetail.jsx 삭제

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,6 @@ import { MainProduct } from "./MainProduct.jsx";
 import { ProductList } from "./product/ProductList.jsx";
 import Stamp from "./event/Stamp.jsx";
 import { theme } from "./component/css/Theme.jsx";
-import { ProductDetail } from "./product/ProductDetail.jsx";
 
 const router = createBrowserRouter([
   {
@@ -17,7 +16,6 @@ const router = createBrowserRouter([
       { index: true, element: <MainProduct /> },
       { path: "product", element: <ProductUpload /> },
       { path: "product/list", element: <ProductList /> },
-      { path: "product/:productId", element: <ProductDetail /> },
       // event
       { path: "/stamp", element: <Stamp /> },
     ],
--- frontend/src/product/ProductDetail.jsx
+++
@@ -1,7 +0,0 @@
-import { Box } from "@chakra-ui/react";
-import { useParams } from "react-router-dom";
-
-export function ProductDetail() {
-  const { productId } = useParams();
-  return <Box>{productId}</Box>;
-}
