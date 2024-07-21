96aa99c20922a3b3df55b5ae6f8c4a8e2db60d69
jnn-jnn1 <anj72005@gmail.com>
2024-07-21 23:24:15 +0900

    react : 주문 조회 경로 APP에 추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import {ChakraProvider} from "@chakra-ui/react";
-import {Home} from "./Home.jsx";
-import {ProductUpload} from "./product/ProductUpload.jsx";
-import {MainProduct} from "./MainProduct.jsx";
-import {ProductList} from "./product/ProductList.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import { Home } from "./Home.jsx";
+import { ProductUpload } from "./product/ProductUpload.jsx";
+import { MainProduct } from "./MainProduct.jsx";
+import { ProductList } from "./product/ProductList.jsx";
 import Stamp from "./event/Stamp.jsx";
-import {theme} from "./component/css/Theme.jsx";
-import {Order} from "./order/Order.jsx";
+import { theme } from "./component/css/Theme.jsx";
+import { Order } from "./order/Order.jsx";
+import { OrderList } from "./order/OrderList.jsx";
 
 const router = createBrowserRouter([
   {
@@ -20,7 +21,8 @@ const router = createBrowserRouter([
       // event
       { path: "/stamp", element: <Stamp /> },
       // order
-      {path:"/order", element: <Order/>}
+      { path: "/order", element: <Order /> },
+      { path: "/order/list", element: <OrderList /> },
     ],
   },
 ]);
