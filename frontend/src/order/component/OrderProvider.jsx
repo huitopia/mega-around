cc9a83d72286ac46977c94d24f776d2007fd8bd2
jnn-jnn1 <anj72005@gmail.com>
2024-07-22 22:09:29 +0900

    react : 주문하기, 바로주문 Context 수정

--- frontend/src/order/component/OrderProvider.jsx
+++ frontend/src/order/component/OrderProvider.jsx
@@ -3,22 +3,40 @@ import { createContext, useState } from "react";
 export const OrderContext = createContext({});
 
 export function OrderProvider({ children }) {
-  const [id, setId] = useState(1);
+  const [productId, setProductId] = useState(1);
+  const [productName, setProductName] = useState("");
+  const [title, setTitle] = useState("");
   const [count, setCount] = useState(1);
   const [totalPrice, setTotalPrice] = useState(0);
   const [option, setOption] = useState([]);
+  const [optionList, setOptionList] = useState([]);
+  const [src, setSrc] = useState("");
+  const [branchName, setBranchName] = useState("");
+  const [branchId, setBranchId] = useState(1);
 
   return (
     <OrderContext.Provider
       value={{
-        id,
-        setId,
+        productId,
+        setProductId,
+        title,
+        setTitle,
         count,
         setCount,
         totalPrice,
         setTotalPrice,
+        // option_item.id List
         option,
         setOption,
+        // option_item.content List
+        optionList,
+        setOptionList,
+        src,
+        setSrc,
+        branchName,
+        setBranchName,
+        branchId,
+        setBranchId,
       }}
     >
       {children}
