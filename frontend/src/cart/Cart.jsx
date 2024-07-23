98da2d73af6f40011cc4642863c8ae7f00081ee4
jnn-jnn1 <anj72005@gmail.com>
2024-07-24 03:50:26 +0900

    react : 장바구니 수량 변경

--- frontend/src/cart/Cart.jsx
+++ frontend/src/cart/Cart.jsx
@@ -1,53 +1,7 @@
-import {
-  Box,
-  Button,
-  Divider,
-  Flex,
-  Heading,
-  Image,
-  Spacer,
-  Spinner,
-  useToast,
-} from "@chakra-ui/react";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { Box, Divider, Heading } from "@chakra-ui/react";
+import { CartProductComp } from "./component/CartProductComp.jsx";
 
 export function Cart() {
-  const [cart, setCart] = useState(null);
-  const [product, setProduct] = useState([]);
-  const toast = useToast();
-
-  useEffect(() => {
-    axios.get("/api/carts").then((res) => {
-      setCart(res.data);
-      setProduct(res.data.cartProduct);
-      console.log(res.data);
-    });
-  }, []);
-
-  if (cart === null) {
-    return <Spinner />;
-  }
-
-  function handleRemoveProduct(productId) {
-    axios
-      .delete(`/api/carts/${productId}`)
-      .then(() =>
-        toast({
-          description: "삭제되었습니다",
-          duration: 1000,
-          status: "success",
-        }),
-      )
-      .catch(() =>
-        toast({
-          description: "삭제 실패했습니다",
-          duration: 1000,
-          status: "error",
-        }),
-      );
-  }
-
   return (
     <Box maxWidth="1000px" mx={"auto"}>
       <Box>
@@ -59,48 +13,8 @@ export function Cart() {
           이대역사거리점
         </Box>
         <Box>주문 상품</Box>
-        <Box>
-          {cart.cartProduct.map((product, index) => (
-            <Flex key={index}>
-              <Image
-                w="50px"
-                h={"50px"}
-                src={
-                  "https://huistudybucket01.s3.ap-northeast-2.amazonaws.com/" +
-                  product.filePath
-                }
-              />
-              <Box>
-                <Box>{product.productName}</Box>
-                {product.optionList.map((item, index) => (
-                  <Box key={index}>{item}</Box>
-                ))}
-                <Flex alignItems="center" mt={2}>
-                  <Button>-</Button>
-                  <Box mx={4} textAlign="center">
-                    {product.count}개
-                  </Box>
-                  <Button>+</Button>
-                </Flex>
-              </Box>
-              <Box>
-                <Button
-                  onClick={() =>
-                    handleRemoveProduct(cart.cartProduct[index].productId)
-                  }
-                >
-                  X
-                </Button>
-                <Box>{product.totalPrice.toLocaleString("ko-KR")}원</Box>
-              </Box>
-            </Flex>
-          ))}
-        </Box>
+        <CartProductComp />
       </Box>
-      <Flex>
-        <Box>상품금액</Box>
-        <Spacer />
-      </Flex>
     </Box>
   );
 }
