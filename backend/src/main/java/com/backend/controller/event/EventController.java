fd61094faeb0f8028037cdd270b68a992f11c9c5
Jin Jin <anj72005@gmail.com>
2024-07-23 11:48:47 +0900

    spring : 스탬프/쿠폰 조회 로직 추가

--- backend/src/main/java/com/backend/controller/event/EventController.java
+++ backend/src/main/java/com/backend/controller/event/EventController.java
@@ -3,6 +3,8 @@ package com.backend.controller.event;
 import com.backend.service.event.EventService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,9 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class EventController {
     private final EventService eventService;
 
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/event/{item}")
-    public ResponseEntity getEvent(@PathVariable String item) {
-        eventService.getEvent(item);
-        return null;
+    public ResponseEntity getEvent(@PathVariable String item, Authentication authentication) {
+        if(eventService.checkItem(item)){
+//        Integer count = eventService.getEvent(item, Integer.valueOf(authentication.getName()));
+            Integer count = eventService.getEvent(item, 1);
+            return ResponseEntity.ok(count);
+        }
+        return ResponseEntity.badRequest().build();
     }
 }
