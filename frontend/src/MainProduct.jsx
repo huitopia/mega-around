aa322ff229fdf31eff247007c13bd97c93c55b5f
Hwayeong <joid1004@naver.com>
2024-07-23 16:57:38 +0900

    navbar,main에 프로필 표시

--- frontend/src/MainProduct.jsx
+++ frontend/src/MainProduct.jsx
@@ -1,3 +1,18 @@
+import { Box, Center } from "@chakra-ui/react";
+import { useContext } from "react";
+import { LoginContext } from "./LoginProvider.jsx";
+
 export function MainProduct() {
-  return <>메가 어라운드 메인페이지입니다. </>;
+  const account = useContext(LoginContext);
+  return (
+    <>
+      <Center p={20} flexDirection={"column"}>
+        <Box fontWeight={"bold"}>
+          {account.nickName}
+          {account.branchName}&nbsp;님
+        </Box>
+        <Box mt={5}>메가 어라운드 메인페이지입니다.</Box>
+      </Center>
+    </>
+  );
 }
--- frontend/src/component/Navbar.jsx
+++ frontend/src/component/Navbar.jsx
@@ -2,10 +2,13 @@ import { Box, Flex, Spacer } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { LoginContext } from "../LoginProvider.jsx";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 export function Navbar() {
   const account = useContext(LoginContext);
   const navigate = useNavigate();
+  console.log(account);
   return (
     <Flex
       h={"50px"}
@@ -19,25 +22,36 @@ export function Navbar() {
         Home
       </Box>
       <Spacer />
-      <Box onClick={() => navigate("/signup")} cursor={"pointer"} mr={"10"}>
-        회원가입
-      </Box>
+
       {!account.isLoggedIn() ? (
-        <Box
-          marginRight="100px"
-          onClick={() => navigate("/login")}
-          cursor={"pointer"}
-        >
-          로그인
-        </Box>
+        <>
+          <Box onClick={() => navigate("/signup")} cursor={"pointer"} mr={"10"}>
+            회원가입
+          </Box>
+          <Box
+            marginRight="100px"
+            onClick={() => navigate("/login")}
+            cursor={"pointer"}
+          >
+            로그인
+          </Box>
+        </>
       ) : (
-        <Box
-          marginRight="100px"
-          onClick={() => account.logout()}
-          cursor={"pointer"}
-        >
-          로그아웃
-        </Box>
+        <>
+          <Box mr={10}>
+            <FontAwesomeIcon icon={faUser} style={{ color: "#FFD43B" }} />
+            &nbsp;
+            {account.nickName}
+            {account.branchName}
+          </Box>
+          <Box
+            marginRight="100px"
+            onClick={() => account.logout()}
+            cursor={"pointer"}
+          >
+            로그아웃
+          </Box>
+        </>
       )}
     </Flex>
   );
