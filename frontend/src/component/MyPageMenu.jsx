b195559a118376e83afb8fec24a010b94585359b
jnn-jnn1 <anj72005@gmail.com>
2024-07-25 03:48:07 +0900

    react : mypage 메뉴

---
+++ frontend/src/component/MyPageMenu.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Menu, MenuButton, MenuItem, MenuList, Text } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+
+function MyPageMenu(props) {
+  const navigate = useNavigate();
+
+  return (
+    <Menu>
+      <MenuButton as={Text} cursor={"pointer"}>
+        마이페이지
+      </MenuButton>
+      <MenuList>
+        <MenuItem onClick={() => navigate("/stamp")}>스탬프</MenuItem>
+        <MenuItem onClick={() => navigate("/coupon")}>쿠폰</MenuItem>
+        <MenuItem onClick={() => navigate(`/order/list`)}>주문 내역</MenuItem>
+      </MenuList>
+    </Menu>
+  );
+}
+
+export default MyPageMenu;
