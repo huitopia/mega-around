ba741c0ee5b600339e1e7f0641ae19aca90422d0
huitopia <huitopia27@gmail.com>
2024-07-25 12:27:07 +0900

    Spring : int deleteStamp(Integer customerId); 추가

--- backend/src/main/java/com/backend/mapper/event/EventMapper.java
+++ backend/src/main/java/com/backend/mapper/event/EventMapper.java
@@ -13,30 +13,31 @@ public interface EventMapper {
     Integer selectStampByCustomerId(Integer customerId);
 
     @Select("""
-    SELECT count
-    FROM coupon
-    WHERE customer_id = #{customerId}
-""")
+                SELECT count
+                FROM coupon
+                WHERE customer_id = #{customerId}
+            """)
     Integer selectCouponByCustomerId(Integer customerId);
 
     @Insert("""
-    INSERT INTO stmap
-    (count) VALUES (#{count})
-    WHERE customer_id = customerId
-    """)
+            INSERT INTO stmap
+            (count) VALUES (#{count})
+            WHERE customer_id = customerId
+            """)
     int insertStamp(Integer customerId);
 
     @Insert("""
-    INSERT INTO coupon
-    (count) VALUES (#{count})
-    WHERE customer_id = customerId
-""")
+                INSERT INTO coupon
+                (count) VALUES (#{count})
+                WHERE customer_id = customerId
+            """)
     int insertCoupon(Integer customerId);
 
     @Delete("""
-    DELETE FROM stamp
-    WHERE customer_id = #{customerId}
-""")
+                DELETE FROM stamp
+                WHERE customer_id = #{customerId}
+            """)
+    int deleteStamp(Integer customerId);
 
     @Delete("""
             DELETE FROM coupon
@@ -45,22 +46,22 @@ public interface EventMapper {
     int deleteCoupon(Integer customerId);
 
     @Update("""
-    UPDATE coupon
-    SET count = #{count}
-    WHERE customer_id = #{customerId}
-""")
+                UPDATE coupon
+                SET count = #{count}
+                WHERE customer_id = #{customerId}
+            """)
     int updateCoupon(Integer customerId);
 
     @Insert("""
-    INSERT INTO notice
-    (customer_id, tag, content) VALUES (#{customerId}, #{tag}, #{content})
-""")
+                INSERT INTO notice
+                (customer_id, tag, content) VALUES (#{customerId}, #{tag}, #{content})
+            """)
     int insertNotice(Integer customerId, Integer tag, Integer content);
 
     @Select("""
-    SELECT *
-    FROM notice
-    WHERE customer_id = #{customerId}
-""")
+                SELECT *
+                FROM notice
+                WHERE customer_id = #{customerId}
+            """)
     Notice selectNoticeByCustomerId(Integer customerId);
 }
