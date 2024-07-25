5ef28c8bf0b23a4fc41cecabc4ad5d0c7fc0c90f
Jin Jin <anj72005@gmail.com>
2024-07-25 17:45:29 +0900

    spring : oauth2ResourceServer 추가

--- backend/src/main/java/com/backend/config/AppConfiguration.java
+++ backend/src/main/java/com/backend/config/AppConfiguration.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
@@ -77,6 +78,7 @@ public class AppConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable());
+        http.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
         return http.build();
     }
 }
