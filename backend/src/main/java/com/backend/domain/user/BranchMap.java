e175ade1a6a373c9f0913647766eb4d68920eb37
huitopia <huitopia27@gmail.com>
2024-07-25 12:03:28 +0900

    Spring : Branch Map DTO 이름 변경

--- backend/src/main/java/com/backend/domain/user/BranchMap.java
+++ backend/src/main/java/com/backend/domain/user/BranchMap.java
@@ -3,8 +3,14 @@ package com.backend.domain.user;
 import lombok.Data;
 
 @Data
-public class Map {
+public class BranchMap {
     private Integer branchId;
     private Double latitude; // 위도
     private Double longitude; // 경도
+
+    public void setBranchId(Branch branch) {
+        this.branchId = branch.getId();
+    }
 }
+
+
