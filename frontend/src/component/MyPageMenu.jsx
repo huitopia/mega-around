4639d1cb71450729b2954eed80c1b7667623416a
jnn-jnn1 <anj72005@gmail.com>
2024-07-26 18:04:03 +0900

    react : 장바구니 조회 mypage 메뉴에 추가

--- frontend/src/component/MyPageMenu.jsx
+++ frontend/src/component/MyPageMenu.jsx
@@ -14,6 +14,7 @@ function MyPageMenu(props) {
         <MenuItem onClick={() => navigate("/stamp")}>스탬프</MenuItem>
         <MenuItem onClick={() => navigate("/coupon")}>쿠폰</MenuItem>
         <MenuItem onClick={() => navigate(`/order/list`)}>주문 내역</MenuItem>
+        <MenuItem onClick={() => navigate(`/cart`)}>장바구니</MenuItem>
       </MenuList>
     </Menu>
   );
