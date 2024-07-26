939db4c5f09cb44850ff61c334678e78ca8354e6
jnn-jnn1 <anj72005@gmail.com>
2024-07-26 23:58:26 +0900

    spring : 주문내역 리스트 역순으로 조회

--- backend/src/main/java/com/backend/mapper/order/OrderMapper.java
+++ backend/src/main/java/com/backend/mapper/order/OrderMapper.java
@@ -27,24 +27,27 @@ public interface OrderMapper {
     // TODO.나중에 시간 제약 추가
     @Select("""
                 <script>
-                SELECT oi.id, oi.customer_id, oi.branch_id, oi.total_price, oi.state_id, oi.created_at, oi.is_take_out, b.branch_name
-                FROM order_item oi JOIN branch b ON oi.branch_id = b.id
-                <if test="stateId == null">
-                    WHERE oi.customer_id = #{customerId}
-                    <if test="period == 'week'">
-                        AND oi.created_at >= NOW() - INTERVAL 1 WEEK;
-                    </if>
-                    <if test="period == 'month'">
-                        AND oi.created_at >= NOW() - INTERVAL 1 MONTH;
-                    </if>
-                    <if test="period == '3-month'">
-                        AND oi.created_at >= NOW() - INTERVAL 3 MONTH;
-                    </if>
-                </if>
-                <if test="stateId != null and branchId != null">
-                    WHERE oi.state_id = #{stateId} AND oi.branch_id = #{branchId};
-                </if>
-                </script>
+                             SELECT oi.id, oi.customer_id, oi.branch_id, oi.total_price, oi.state_id, oi.created_at, oi.is_take_out, b.branch_name
+                             FROM order_item oi
+                             JOIN branch b ON oi.branch_id = b.id
+                             WHERE 1 = 1
+                             <if test="stateId == null">
+                                 AND oi.customer_id = #{customerId}
+                                 <if test="period == 'week'">
+                                     AND oi.created_at >= NOW() - INTERVAL 1 WEEK
+                                 </if>
+                                 <if test="period == 'month'">
+                                     AND oi.created_at >= NOW() - INTERVAL 1 MONTH
+                                 </if>
+                                 <if test="period == '3-month'">
+                                     AND oi.created_at >= NOW() - INTERVAL 3 MONTH
+                                 </if>
+                             </if>
+                             <if test="stateId != null and branchId != null">
+                                 AND oi.state_id = #{stateId} AND oi.branch_id = #{branchId}
+                             </if>
+                             ORDER BY oi.id DESC
+                         </script>
             """)
     List<OrderItem> selectOrderItemList(Integer customerId, String period, Integer stateId, Integer branchId);
 
@@ -71,16 +74,16 @@ public interface OrderMapper {
     int updateOrderItemState(Integer id, Integer stateId);
 
     @Select("""
-    SELECT id
-    FROM payment
-    WHERE order_item_id = #{orderItemId}
-""")
+                SELECT id
+                FROM payment
+                WHERE order_item_id = #{orderItemId}
+            """)
     Integer selectPaymentIdByOrderId(Integer orderItemId);
 
     @Select("""
-    SELECT oi.id, oi.customer_id, oi.branch_id, oi.total_price, oi.state_id, oi.created_at, oi.is_take_out, b.branch_name
-                FROM order_item oi JOIN branch b ON oi.branch_id = b.id
-                WHERE oi.id = #{id}
-""")
+                SELECT oi.id, oi.customer_id, oi.branch_id, oi.total_price, oi.state_id, oi.created_at, oi.is_take_out, b.branch_name
+                            FROM order_item oi JOIN branch b ON oi.branch_id = b.id
+                            WHERE oi.id = #{id}
+            """)
     OrderItem selectOrderItemByOrderId(Integer id);
 }
