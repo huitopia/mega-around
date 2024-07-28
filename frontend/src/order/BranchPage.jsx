35691192ea0970b9b04707e37b68152969b58e3d
jnn-jnn1 <anj72005@gmail.com>
2024-07-28 20:37:36 +0900

    react : 판매자 페이지 주문 정보 표시

--- frontend/src/order/BranchPage.jsx
+++ frontend/src/order/BranchPage.jsx
@@ -1,7 +1,10 @@
 import { Box, Divider, Heading } from "@chakra-ui/react";
 import { OrderManageComp } from "./component/OrderManageComp.jsx";
+import { useState } from "react";
 
 export function BranchPage() {
+  const [isChanged, setIsChanged] = useState(false);
+
   return (
     <Box maxWidth="1000px" mx={"auto"}>
       <Box>
@@ -9,9 +12,27 @@ export function BranchPage() {
       </Box>
       <Divider border={"1px solid black"} my={4} />
       <Box>
-        <OrderManageComp stateId={0} branchId={1} text={"신규주문"}/>
-        <OrderManageComp stateId={1} branchId={1} text={"제조중"}/>
-        <OrderManageComp stateId={2} branchId={1} text={"제조완료"}/>
+        <OrderManageComp
+          stateId={1}
+          branchId={1}
+          text={"신규주문"}
+          isChanged={isChanged}
+          setIsChanged={setIsChanged}
+        />
+        <OrderManageComp
+          stateId={2}
+          branchId={1}
+          text={"제조중"}
+          isChanged={isChanged}
+          setIsChanged={setIsChanged}
+        />
+        <OrderManageComp
+          stateId={3}
+          branchId={1}
+          text={"제조완료"}
+          isChanged={isChanged}
+          setIsChanged={setIsChanged}
+        />
       </Box>
     </Box>
   );
