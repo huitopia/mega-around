34841a838651daa8fd921c744d183daf5c2b2426
jnn-jnn1 <anj72005@gmail.com>
2024-07-28 21:48:58 +0900

    react : 쿠폰, 스탬프 페이지 생성

--- frontend/src/event/Coupon.jsx
+++ frontend/src/event/Coupon.jsx
@@ -1,12 +1,32 @@
 import { Box, Divider, Heading } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export function Coupon() {
+  const [couponCount, setCouponCount] = useState(0);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    axios("/api/event/coupon")
+      .then((res) => {
+        setCouponCount(res.data);
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
   return (
     <Box maxWidth="1000px" mx={"auto"}>
       <Box>
         <Heading>Coupon</Heading>
       </Box>
       <Divider border={"1px solid black"} my={4} />
+      {loading ? (
+        <Box>Loading...</Box>
+      ) : couponCount > 0 ? (
+        <Box>보유한 쿠폰 수: {couponCount}</Box>
+      ) : (
+        <Box>보유한 쿠폰이 없습니다.</Box>
+      )}{" "}
     </Box>
   );
 }
--- frontend/src/event/Stamp.jsx
+++ frontend/src/event/Stamp.jsx
@@ -1,37 +1,98 @@
-import React from "react";
-import { Box, Button, Flex, Grid, Heading, Spacer } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Grid,
+  Heading,
+  Image,
+  Spacer,
+  Spinner,
+  Text,
+} from "@chakra-ui/react";
+import axios from "axios";
 
 function Stamp(props) {
   const boxes = Array.from({ length: 10 }, (_, i) => i + 1);
+  const [stampCount, setStampCount] = useState(null);
+
+  useEffect(() => {
+    axios.get("/api/event/stamp").then((res) => {
+      setStampCount(res.data);
+    });
+  }, []);
+
+  if (stampCount === null) {
+    return <Spinner />;
+  }
 
   return (
-    <Box>
+    <Box maxWidth="1000px" mx={"auto"}>
+      <Box>
+        <Heading>Stamp</Heading>
+      </Box>
+      <Divider border={"1px solid black"} my={4} />
       <Flex>
-        <Heading>스탬프</Heading>
+        <Box bgColor={"yellow"} w={"520px"} h={"300px"} borderRadius={"20px"}>
+          <Flex alignItems="center" height="300" justifyContent="center">
+            <Grid templateColumns="repeat(5, 1fr)" gap={7} alignItems="center">
+              {boxes.map((box) => (
+                <Box key={box}>
+                  {stampCount < box ? (
+                    <Box
+                      bg="rgba(0, 0, 0, 0.1)"
+                      borderRadius="full"
+                      w="50px"
+                      h="50px"
+                      display="flex"
+                      alignItems="center"
+                      justifyContent="center"
+                      color="white"
+                    >
+                      {box}
+                    </Box>
+                  ) : (
+                    <Box
+                      position="relative"
+                      display="inline-block"
+                      cursor="pointer"
+                      _hover={{ ".description": { opacity: 1 } }}
+                    >
+                      <Image
+                        w="50px"
+                        h="80px"
+                        src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbXtHba%2FbtsICfOOFVO%2Fg6xk09ztnHMPKMJQKEuYjK%2Fimg.png"
+                      />
+                      <Box
+                        w={"100px"}
+                        h={"20px"}
+                        className="description"
+                        position="absolute"
+                        top="20"
+                        left="-5"
+                        right="0"
+                        bottom="0"
+                        bg="rgba(0, 0, 0, 0.5)"
+                        color="white"
+                        display="flex"
+                        alignItems="center"
+                        justifyContent="center"
+                        opacity="0"
+                        transition="opacity 0.3s ease"
+                      >
+                        <Text fontSize="sm">설명</Text>
+                      </Box>
+                    </Box>
+                  )}
+                </Box>
+              ))}
+            </Grid>
+          </Flex>
+        </Box>
         <Spacer />
         <Button>적립내역 보기</Button>
       </Flex>
-      <Box bgColor={"yellow"} w={"520px"} h={"300px"} borderRadius={"20px"}>
-        <Flex alignItems="center" height="300" justifyContent="center">
-          <Grid templateColumns="repeat(5, 1fr)" gap={7} alignItems="center">
-            {boxes.map((box, index) => (
-              <Box
-                bg="rgba(0, 0, 0, 0.1)"
-                borderRadius={"full"}
-                key={index}
-                w="50px"
-                h="50px"
-                display="flex"
-                alignItems="center"
-                justifyContent="center"
-                color="white"
-              >
-                {box}
-              </Box>
-            ))}
-          </Grid>
-        </Flex>
-      </Box>
     </Box>
   );
 }
