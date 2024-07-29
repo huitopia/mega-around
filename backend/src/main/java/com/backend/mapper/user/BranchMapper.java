6fe5ad36dbaaff94236220e31c862381c3989c0b
huitopia <huitopia27@gmail.com>
2024-07-30 00:53:52 +0900

    SPRING : 현재 위치 기준으로 가까운 지점 10곳 받아옴

--- backend/src/main/java/com/backend/mapper/user/BranchMapper.java
+++ backend/src/main/java/com/backend/mapper/user/BranchMapper.java
@@ -3,6 +3,9 @@ package com.backend.mapper.user;
 import com.backend.domain.user.BranchGeocode;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface BranchMapper {
@@ -11,4 +14,13 @@ public interface BranchMapper {
             VALUES (#{branchId}, #{latitude}, #{longitude})
             """)
     int insertGeocodeById(BranchGeocode branchGeocode);
+
+    @Select("""
+            SELECT branch_id, latitude, longitude,
+                (6371 * acos(cos(radians(#{lat})) * cos(radians(latitude)) * cos(radians(longitude) - radians(#{lng}))
+                + sin(radians(#{lat})) * sin(radians(latitude)))) AS distance
+            FROM branch_geocode
+            ORDER BY distance LIMIT 10
+            """)
+    List<BranchGeocode> selectAroundBranchList(double lat, double lng);
 }
