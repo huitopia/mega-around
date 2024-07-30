1ce7850607c142eb6191d23fdec3e89de9aa069d
jnn-jnn1 <anj72005@gmail.com>
2024-07-31 00:03:55 +0900

    spring : 알림 조회 기능 추가

--- backend/src/main/java/com/backend/mapper/event/EventMapper.java
+++ backend/src/main/java/com/backend/mapper/event/EventMapper.java
@@ -3,6 +3,8 @@ package com.backend.mapper.event;
 import com.backend.domain.event.Notice;
 import org.apache.ibatis.annotations.*;
 
+import java.util.List;
+
 @Mapper
 public interface EventMapper {
 
@@ -61,8 +63,10 @@ public interface EventMapper {
                 SELECT *
                 FROM notice
                 WHERE customer_id = #{customerId}
+                AND tag = #{tag}
+                ORDER BY created_at DESC
             """)
-    Notice selectNoticeByCustomerId(Integer customerId);
+    List<Notice> selectNoticeByCustomerId(Integer customerId, String tag);
 
     @Update("""
                 UPDATE stamp
