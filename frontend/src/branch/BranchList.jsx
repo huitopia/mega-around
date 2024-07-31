c4bf3212e6e23407935a889ae060bf9f9028bb31
huitopia <huitopia27@gmail.com>
2024-07-31 16:37:32 +0900

    REACT : 지점 카드 클릭 시 모달 오픈 후 주문하기 버튼 누르면 상품리스트로 이동하며 지점아이디 전달

--- frontend/src/branch/BranchList.jsx
+++ frontend/src/branch/BranchList.jsx
@@ -1,24 +1,38 @@
 import {
   Box,
+  Button,
   Card,
   CardBody,
   CardHeader,
   Flex,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
   SimpleGrid,
   Spacer,
   Spinner,
   Text,
+  useDisclosure,
 } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export const BranchList = () => {
   // const kakao_javasciprt_key = import.meta.env.VITE_KAKAO_JAVASCRIPT_KEY;
 
   // 현재 위치
   const [location, setLocation] = useState(null);
+  const [selectedBranchId, setSelectedBranchId] = useState(0);
+  const [selectedBranchName, setSelectedBranchName] = useState("");
   const [branches, setBranches] = useState([]);
   const [error, setError] = useState(null);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchLocationAndStores = async () => {
@@ -80,6 +94,9 @@ export const BranchList = () => {
           position: position,
         });
         marker.setMap(map);
+        window.kakao.maps.event.addListener(marker, "click", () => {
+          setSelectedBranchId(branch.branchId);
+        });
       });
     }
   }, [location, branches]);
@@ -113,7 +130,20 @@ export const BranchList = () => {
           >
             <SimpleGrid spacing={4} columns={2}>
               {branches.map((branch) => (
-                <Card key={branch.branchId} height={"120px"}>
+                <Card
+                  key={branch.branchId}
+                  height={"120px"}
+                  backgroundColor={
+                    branch.branchId === selectedBranchId && "yellow"
+                  }
+                  cursor={"pointer"}
+                  _hover={{ backgroundColor: "yellow" }}
+                  onClick={() => {
+                    setSelectedBranchName(branch.branchName);
+                    setSelectedBranchId(branch.branchId);
+                    onOpen();
+                  }}
+                >
                   <CardHeader>
                     <Text size={"md"} as={"b"}>
                       {branch.branchName}
@@ -136,6 +166,37 @@ export const BranchList = () => {
           </Box>
         </>
       )}
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{selectedBranchName}에서 주문하시겠습니까?</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody textColor={"red"}>
+            주문 확인 후 취소가 불가합니다.
+          </ModalBody>
+          <ModalFooter justifyContent="center">
+            <Button
+              width={"40%"}
+              colorScheme="pink"
+              onClick={onClose}
+              ml={"5%"}
+            >
+              취소
+            </Button>
+            <Spacer />
+            <Button
+              width={"40%"}
+              colorScheme={"orange"}
+              mr={"5%"}
+              onClick={() => {
+                navigate(`/product/list?branchId=${selectedBranchId}`);
+              }}
+            >
+              주문하기
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 };
