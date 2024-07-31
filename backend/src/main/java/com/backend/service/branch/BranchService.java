d282fbf48afdb05ced6b9813959a44e8d59b3fd4
huitopia <huitopia27@gmail.com>
2024-07-31 15:20:03 +0900

    SPRING : 주석 제거

--- backend/src/main/java/com/backend/service/branch/BranchService.java
+++ backend/src/main/java/com/backend/service/branch/BranchService.java
@@ -40,7 +40,7 @@ public class BranchService {
     }
 
     private double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
-        final int R = 6371; // 지구 반경 (km)
+        final int R = 6371;
         double latDistance = Math.toRadians(lat2 - lat1);
         double lonDistance = Math.toRadians(lon2 - lon1);
         double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
