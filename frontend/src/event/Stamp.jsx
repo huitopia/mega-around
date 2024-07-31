b843342d4917be07f5fd620df8c328b37fd2a7ca
Jin Jin <anj72005@gmail.com>
2024-07-31 10:59:14 +0900

    react : 스탬프 페이지 css 수정

--- frontend/src/event/Stamp.jsx
+++ frontend/src/event/Stamp.jsx
@@ -19,15 +19,32 @@ import { StampModal } from "./component/StampModal.jsx";
 function Stamp(props) {
   const boxes = Array.from({ length: 10 }, (_, i) => i + 1);
   const [stampCount, setStampCount] = useState(null);
+  const [stampDate, setStampDate] = useState(null);
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   useEffect(() => {
     axios.get("/api/event/stamp").then((res) => {
       setStampCount(res.data);
     });
+    axios.get("/api/notice/stamp").then(res => {
+      function createDate(data) {
+        const result = [];
+        data.map((item) => {
+            const arr = item.content.split(" ");
+            const count = parseInt(arr[1].replace("개",""));
+            for (let i = 0; i < count; i++) {
+              result.push(item.createdAtString);
+            }
+        }
+          )
+        return result;
+      }
+
+      const date = createDate(res.data);
+      setStampDate(date)});
   }, []);
 
-  if (stampCount === null) {
+  if (stampCount === null || stampDate === null) {
     return <Spinner />;
   }
 
@@ -37,7 +54,15 @@ function Stamp(props) {
         <Heading>Stamp</Heading>
       </Box>
       <Divider border={"1px solid black"} my={4} />
-      <Flex>
+      <Flex justify={"right"}>
+        <Flex gap={2}>
+          <Heading>스탬프</Heading>
+          <Heading color={"red"}>{stampCount}</Heading>
+        </Flex>
+        <Spacer/>
+        <Button onClick={() => onOpen()} colorScheme={"orange"} borderRadius={"20px"} w={"170px"}>적립내역 보기</Button>
+      </Flex>
+      <Flex justify={"center"} mt={10}>
         <Box bgColor={"yellow"} w={"520px"} h={"300px"} borderRadius={"20px"}>
           <Flex alignItems="center" height="300" justifyContent="center">
             <Grid templateColumns="repeat(5, 1fr)" gap={7} alignItems="center">
@@ -69,12 +94,12 @@ function Stamp(props) {
                         src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbXtHba%2FbtsICfOOFVO%2Fg6xk09ztnHMPKMJQKEuYjK%2Fimg.png"
                       />
                       <Box
-                        w={"100px"}
-                        h={"20px"}
+                        w={"150px"}
+                        h={"40px"}
                         className="description"
                         position="absolute"
-                        top="20"
-                        left="-5"
+                        top="12"
+                        left="-10"
                         right="0"
                         bottom="0"
                         bg="rgba(0, 0, 0, 0.5)"
@@ -84,8 +109,11 @@ function Stamp(props) {
                         justifyContent="center"
                         opacity="0"
                         transition="opacity 0.3s ease"
+                        zIndex={1}
+                        borderRadius={"10px"}
                       >
-                        <Text fontSize="sm">설명</Text>
+                        <Text fontSize="xs">  발급일: {stampDate[box-1]}
+                        </Text>
                       </Box>
                     </Box>
                   )}
@@ -94,12 +122,10 @@ function Stamp(props) {
             </Grid>
           </Flex>
         </Box>
-        <Spacer />
-        <Button onClick={() => onOpen()}>적립내역 보기</Button>
       </Flex>
-      <Box bg={"#f8f9fa"}>
-        <Text>유의사항</Text>
-        <Box whiteSpace="pre-line">{longText}</Box>
+      <Box bg={"#f8f9fa"} mt={12} p={7}>
+        <Text fontWeight={"bold"}>유의사항</Text>
+        <Box whiteSpace="pre-line" fontSize={"sm"} lineHeight={2}>{longText}</Box>
       </Box>
       <StampModal isOpen={isOpen} onClose={onClose} />
     </Box>
