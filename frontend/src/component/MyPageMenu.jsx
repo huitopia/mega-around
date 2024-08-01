74f13bc993580b5046dd4c157e4efd528be8a723
huitopia <huitopia27@gmail.com>
2024-08-01 23:01:10 +0900

    react : 닉네임 부분 CSS 수정

--- frontend/src/component/MyPageMenu.jsx
+++ frontend/src/component/MyPageMenu.jsx
@@ -1,9 +1,11 @@
 import React, { useContext, useEffect, useState } from "react";
 import {
   Box,
+  Center,
   Flex,
   Menu,
   MenuButton,
+  MenuDivider,
   MenuItem,
   MenuList,
   Popover,
@@ -16,10 +18,11 @@ import {
 } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import { LoginContext } from "./LoginProvider.jsx";
-import { faBell, faCaretDown } from "@fortawesome/free-solid-svg-icons";
+import { faBell } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Client } from "@stomp/stompjs";
 import axios from "axios";
+import { ChevronDownIcon } from "@chakra-ui/icons";
 
 function MyPageMenu({ setIsChanged, updateAlarm }) {
   const navigate = useNavigate();
@@ -68,74 +71,105 @@ function MyPageMenu({ setIsChanged, updateAlarm }) {
     }
   }, [updateAlarm]);
 
-  function getUnreadNoticeCount(item){
-    item
+  function getUnreadNoticeCount(item) {
+    item;
   }
 
   return (
-    <Flex>
-      <Popover>
-        <PopoverTrigger>
-          <Flex alignItems={"center"} mr={2} gap={1}>
-          <Box color={"white"} bg={"red"} w="20px" h={"20px"} borderRadius={"full"} fontSize={"sm"} textAlign={"center"}>{unreadNoticeCount}</Box>
-          <FontAwesomeIcon icon={faBell} />
-          </Flex>
-        </PopoverTrigger>
-        <PopoverContent>
-          <PopoverArrow />
-          <PopoverCloseButton />
-          <PopoverBody>
-            {noticeList && Object.keys(noticeList).length > 0 ? (
-              noticeList.map((notice) => (
-                <Box key={notice.id}>
-                  <Box>{notice.content}</Box>
-                </Box>
-              ))
-            ) : (
-              <Box>보유한 공지가 없습니다.</Box>
+    <Box>
+      <Flex gap={3}>
+        <Menu>
+          <Center>
+            <MenuButton as={Text} cursor={"pointer"} py={1}>
+              {account.nickName}
+              {account.branchName}&nbsp;님&nbsp;&nbsp;
+              <ChevronDownIcon />
+            </MenuButton>
+          </Center>
+          <MenuList fontSize={"sm"}>
+            {account.hasAuth() === "customer" && (
+              <>
+                <MenuItem onClick={() => navigate("/stamp")}>스탬프</MenuItem>
+                <MenuItem onClick={() => navigate("/coupon")}>쿠폰</MenuItem>
+                <MenuItem onClick={() => navigate(`/cart`)}>장바구니</MenuItem>
+                <MenuItem onClick={() => navigate(`/order/list`)}>
+                  주문 내역
+                </MenuItem>
+                <MenuItem
+                  onClick={() => navigate(`/mypage/customer/${account.id}`)}
+                >
+                  내 정보
+                </MenuItem>
+              </>
             )}
-          </PopoverBody>
-        </PopoverContent>
-      </Popover>
-      <Menu>
-        <MenuButton as={Text} cursor={"pointer"}>
-          {account.nickName}
-          {account.branchName}&nbsp;님&nbsp;&nbsp;
-          <FontAwesomeIcon icon={faCaretDown} />
-        </MenuButton>
-
-        <MenuList fontSize={"14px"} width="200px">
-          {account.hasAuth() === "customer" && (
-            <>
-              <MenuItem onClick={() => navigate("/stamp")}>스탬프</MenuItem>
-              <MenuItem onClick={() => navigate("/coupon")}>쿠폰</MenuItem>
-              <MenuItem onClick={() => navigate(`/cart`)}>장바구니</MenuItem>
-              <MenuItem onClick={() => navigate(`/order/list`)}>
-                주문 내역
-              </MenuItem>
+            {account.hasAuth() === "branch" && (
               <MenuItem
-                onClick={() => navigate(`/mypage/customer/${account.id}`)}
+                onClick={() => navigate(`/mypage/branch/${account.id}`)}
               >
-                내 정보
+                내 정보(지점)
               </MenuItem>
-            </>
-          )}
-          {account.hasAuth() === "branch" && (
-            <MenuItem onClick={() => navigate(`/mypage/branch/${account.id}`)}>
-              내 정보(지점)
+            )}
+            {account.hasAuth() === "customer" || (
+              <MenuItem onClick={() => navigate(`/product/list`)}>
+                상품 리스트
+              </MenuItem>
+            )}
+            {account.hasAuth() === "admin" && (
+              <MenuItem onClick={() => navigate(`/product`)}>
+                상품 등록
+              </MenuItem>
+            )}
+            <MenuDivider />
+            <MenuItem
+              onClick={() => {
+                account.logout();
+                navigate("/");
+              }}
+            >
+              Logout
             </MenuItem>
-          )}
-          {account.hasAuth() === "customer" || (
-            <MenuItem onClick={() => navigate(`/product/list`)}>
-              상품 리스트
-            </MenuItem>
-          )}
-          {account.hasAuth() === "admin" && (
-            <MenuItem onClick={() => navigate(`/product`)}>상품 등록</MenuItem>
-          )}
-        </MenuList>
-      </Menu>
-    </Flex>
+          </MenuList>
+        </Menu>
+        <Popover>
+          <PopoverTrigger>
+            <Flex alignItems={"center"} cursor={"pointer"}>
+              <Box>
+                <FontAwesomeIcon
+                  color={unreadNoticeCount > 0 ? "#FDD000" : "rgba(0,0,0,0.5)"}
+                  icon={faBell}
+                />
+              </Box>
+              <Box
+                color={"#444444"}
+                w="20px"
+                h={"50px"}
+                fontSize={"sm"}
+                textAlign={"center"}
+                as={"b"}
+                hidden={unreadNoticeCount === 0}
+              >
+                {unreadNoticeCount}
+              </Box>
+            </Flex>
+          </PopoverTrigger>
+          <PopoverContent>
+            <PopoverArrow />
+            <PopoverCloseButton />
+            <PopoverBody>
+              {noticeList && Object.keys(noticeList).length > 0 ? (
+                noticeList.map((notice) => (
+                  <Box key={notice.id}>
+                    <Box>{notice.content}</Box>
+                  </Box>
+                ))
+              ) : (
+                <Box>보유한 공지가 없습니다.</Box>
+              )}
+            </PopoverBody>
+          </PopoverContent>
+        </Popover>
+      </Flex>
+    </Box>
   );
 }
 
