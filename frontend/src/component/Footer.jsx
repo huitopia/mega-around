71fe9a25cae2fd1963b1e36eef8b1862deb69d67
huitopia <huitopia27@gmail.com>
2024-08-01 21:31:29 +0900

    react : 길이, 색상 변경

--- frontend/src/component/Footer.jsx
+++ frontend/src/component/Footer.jsx
@@ -1,7 +1,8 @@
 import { Box } from "@chakra-ui/react";
 
 export const Footer = () => (
-  <Box backgroundColor={"yellow"} height={"120px"} mt={"100px"} width={"100%"}>
-    Footer marginTop=100px, height=120px
+  <Box backgroundColor={"yellow"} height={"200px"} mt={"100px"} width={"100%"}>
+    <Box height={"50px"} backgroundColor={"#444444"}></Box>
+    <Box height={"150px"}></Box>
   </Box>
 );
