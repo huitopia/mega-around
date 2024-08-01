5f58e735863f6fad2be019b876fe99048f2dab5b
huitopia <huitopia27@gmail.com>
2024-08-01 21:31:48 +0900

    react : paddingBottom delete

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -2,17 +2,17 @@ import { Box } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
 import { Navbar } from "./component/Navbar.jsx";
 import { Footer } from "./component/Footer.jsx";
-import {useState} from "react";
+import { useState } from "react";
 
 export function Home() {
   const [updateAlarm, setUpdateAlarm] = useState(false);
   return (
     <Box h={"100%"}>
       <Box>
-        <Navbar updateAlarm={updateAlarm}/>
+        <Navbar updateAlarm={updateAlarm} />
       </Box>
-      <Box minHeight={"100vh"} position={"relative"} paddingBottom={"70px"}>
-        <Outlet context={{ setUpdateAlarm }}/>
+      <Box minHeight={"100vh"} position={"relative"}>
+        <Outlet context={{ setUpdateAlarm }} />
       </Box>
       <Box>
         <Footer />
