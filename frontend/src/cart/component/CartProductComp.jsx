93006a9896704e4caa8c60770f89ea5478b1a7ca
huitopia <huitopia27@gmail.com>
2024-08-02 14:32:10 +0900

    react : CSS 수정

--- frontend/src/cart/component/CartProductComp.jsx
+++ frontend/src/cart/component/CartProductComp.jsx
@@ -1,11 +1,25 @@
-import {Box, Button, Center, Divider, Flex, Image, Spacer, Spinner, Text} from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Center,
+  Divider,
+  Flex,
+  Heading,
+  Image,
+  Spacer,
+  Spinner,
+  Text,
+} from "@chakra-ui/react";
 import { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import { LoginContext } from "../../component/LoginProvider.jsx";
 import { CustomToast } from "../../component/CustomToast.jsx";
 import { useNavigate } from "react-router-dom";
 import { OrderContext } from "../../order/component/OrderProvider.jsx";
-import {RoundGrayButtonStyle, RoundBlackButtonStyle} from "../../component/css/style.js";
+import {
+  RoundBlackButtonStyle,
+  RoundGrayButtonStyle,
+} from "../../component/css/style.js";
 
 export function CartProductComp(props) {
   const account = useContext(LoginContext);
@@ -23,7 +37,7 @@ export function CartProductComp(props) {
     });
   }, [userId, isChanged]);
 
-  if (cart === null || cart.cartProduct.length === 0) {
+  if (cart === "" || cart === null) {
     return <Box>장바구니에 상품이 없어요</Box>;
   }
 
@@ -74,83 +88,150 @@ export function CartProductComp(props) {
 
   return (
     <Box>
-      {cart.cartProduct.map((product, index) => (
-        <Box  key={index} ml={3} mb={7} mr={6}>
-        <Flex>
-          <Image
-            w="150px"
-            h={"150px"}
-            src={
-              "https://huistudybucket01.s3.ap-northeast-2.amazonaws.com/" +
-              product.filePath
-            }
-          />
-          <Box ml={4} mt={5}>
-            <Box fontWeight={"bold"} fontSize={"23px"} mb={2}>{product.productName}</Box>
-            {product.optionList.map((item, index) => (
-              <Text fontSize={"sm"} key={index} mt={1}>{item}</Text>
-            ))}
-            <Flex alignItems="center" mt={7} mr={12} gap={8}>
-              <Button onClick={() => handleReduceCount(index)} {...RoundBlackButtonStyle}>-</Button>
-              <Box mx={4} textAlign="center" fontSize={"lg"}>
-                {product.count}
-              </Box>
-              <Button onClick={() => handlePlusCount(index)} {...RoundBlackButtonStyle}>+</Button>
-            </Flex>
-          </Box>
-
-          <Spacer/>
-
-          <Box>
-            <Flex justify={"right"}>
-            <Button
-              onClick={() =>
-                handleRemoveProduct(cart.cartProduct[index].productId)
-              }
-              {...RoundGrayButtonStyle}
-              mt={5}
-            >
-              X
-            </Button>
-            </Flex>
-            <Box fontSize={"35px"} fontWeight={"bold"} mt={10}>
-              {(product.totalPrice * product.count).toLocaleString("ko-KR")}원
-            </Box>
-          </Box>
-        </Flex>
-      {cart.cartProduct.length - 1 > index && (
-        <Divider border={"1px solid gray.200"} my={4} ml={3} mr={6} mt={9}/>
-  )}
-  </Box>
-      ))}
-      <Box bg={"#f8f9fa"} h={"30px"} w={"100%"}/>
-      <Flex mt={"60px"} align={"center"}>
-        <Box fontSize={"20px"}>상품금액</Box>
-        <Spacer />
-        <Box fontSize={"25px"} color={"red"} fontWeight={"bold"}>
-          {calculateTotalPrice(cart.cartProduct).toLocaleString("ko-KR")}원
+      <Box
+        height={"280px"}
+        backgroundColor={"#444444"}
+        textAlign={"center"}
+        display={"flex"}
+        alignItems={"center"}
+        justifyContent="center"
+      >
+        <Box>
+          <Heading size="2xl" textColor={"#FDD000"}>
+            CART LIST
+          </Heading>
+          <Text fontSize={"lg"} textColor={"pink"}>
+            {cart.branchName}
+          </Text>
         </Box>
-      </Flex>
-      {cart && (
-        <Center mt={"100px"} mb={"100px"}>
-        <Button
-          colorScheme={"orange"}
-          w={"650px"}
-          h={"60px"}
-          fontSize={"19px"}
-          onClick={() =>
-            axios
-              .put("/api/carts", cart)
-              .then(() => navigate("/order?type=cart"))
-              .catch(() =>
-                errorToast("주문하기 실패했습니다. 다시 시도해주십시오."),
-              )
-          }
+        <Box
+          border={"1px solid blue"}
+          maxWidth={"1200px"}
+          mx={"auto"}
+          mt={"50px"}
         >
-          주문하기
-        </Button>
-        </Center>
-      )}
+          <Box textAlign={"center"}>
+            <Heading size={"lg"}>PRODUCT LIST</Heading>
+          </Box>
+          <Box border={"1px solid black"} mt={"20px"}>
+            <Flex>
+              <Box width={"60%"}>
+                {cart.cartProduct.map((product, index) => (
+                  <Box key={index} ml={"10%"} border={"1px solid red"}>
+                    <Flex>
+                      <Image
+                        w="150px"
+                        h={"150px"}
+                        src={
+                          "https://huistudybucket01.s3.ap-northeast-2.amazonaws.com/" +
+                          product.filePath
+                        }
+                      />
+                      <Box ml={4} mt={5}>
+                        <Box fontWeight={"bold"} fontSize={"23px"} mb={2}>
+                          {product.productName}
+                        </Box>
+                        {product.optionList.map((item, index) => (
+                          <Text fontSize={"sm"} key={index} mt={1}>
+                            {item}
+                          </Text>
+                        ))}
+                        <Flex alignItems="center" mt={7} mr={12} gap={8}>
+                          <Button
+                            onClick={() => handleReduceCount(index)}
+                            {...RoundBlackButtonStyle}
+                          >
+                            -
+                          </Button>
+                          <Box mx={4} textAlign="center" fontSize={"lg"}>
+                            {product.count}
+                          </Box>
+                          <Button
+                            onClick={() => handlePlusCount(index)}
+                            {...RoundBlackButtonStyle}
+                          >
+                            +
+                          </Button>
+                        </Flex>
+                      </Box>
+
+                      <Spacer />
+                      <Spacer />
+
+                      <Box>
+                        <Flex justify={"right"}>
+                          <Button
+                            onClick={() =>
+                              handleRemoveProduct(
+                                cart.cartProduct[index].productId,
+                              )
+                            }
+                            {...RoundGrayButtonStyle}
+                            mt={5}
+                          >
+                            X
+                          </Button>
+                        </Flex>
+                        <Box fontSize={"35px"} fontWeight={"bold"} mt={10}>
+                          {(product.totalPrice * product.count).toLocaleString(
+                            "ko-KR",
+                          )}
+                          원
+                        </Box>
+                      </Box>
+                    </Flex>
+                    {cart.cartProduct.length - 1 > index && (
+                      <Divider
+                        border={"1px solid gray.200"}
+                        my={4}
+                        ml={3}
+                        mr={6}
+                        mt={9}
+                      />
+                    )}
+                  </Box>
+                ))}
+              </Box>
+              <Box width={"30%"} ml={"5%"} border={"1px solid yellow"}>
+                <Flex mt={"60px"} align={"center"}>
+                  <Box fontSize={"20px"}>상품금액</Box>
+                  <Spacer />
+                  <Box fontSize={"25px"} color={"red"} fontWeight={"bold"}>
+                    {calculateTotalPrice(cart.cartProduct).toLocaleString(
+                      "ko-KR",
+                    )}
+                    원
+                  </Box>
+                </Flex>
+                {cart && (
+                  <Center mt={"100px"} mb={"100px"}>
+                    <Button
+                      colorScheme={"orange"}
+                      w={"650px"}
+                      h={"60px"}
+                      fontSize={"19px"}
+                      onClick={() =>
+                        axios
+                          .put("/api/carts", cart)
+                          .then(() => navigate("/order?type=cart"))
+                          .catch(() =>
+                            errorToast(
+                              "주문하기 실패했습니다. 다시 시도해주십시오.",
+                            ),
+                          )
+                      }
+                    >
+                      주문하기
+                    </Button>
+                  </Center>
+                )}
+              </Box>
+            </Flex>
+          </Box>
+          <Box bg={"#f8f9fa"} h={"30px"} w={"100%"} />
+          {/* 옆으로*/}
+        </Box>
+      </Box>
     </Box>
   );
 }
--- frontend/src/order/Order.jsx
+++ frontend/src/order/Order.jsx
@@ -6,10 +6,11 @@ import {
   AccordionPanel,
   Box,
   Button,
+  ButtonGroup,
   Card,
+  Center,
   Checkbox,
   CheckboxGroup,
-  Divider,
   Flex,
   FormControl,
   FormLabel,
@@ -21,12 +22,20 @@ import {
   Spacer,
   Stack,
   StackDivider,
+  Text,
 } from "@chakra-ui/react";
 import { useContext, useEffect, useState } from "react";
 import { ChevronRightIcon } from "@chakra-ui/icons";
-import { checkBoxStyle } from "../component/css/style.js";
+import {
+  checkBoxStyle,
+  RoundBlackButtonStyle,
+} from "../component/css/style.js";
 import axios from "axios";
-import {useNavigate, useOutletContext, useSearchParams} from "react-router-dom";
+import {
+  useNavigate,
+  useOutletContext,
+  useSearchParams,
+} from "react-router-dom";
 import { OrderContext } from "./component/OrderProvider.jsx";
 import { LoginContext } from "../component/LoginProvider.jsx";
 import { CustomToast } from "../component/CustomToast.jsx";
@@ -94,7 +103,6 @@ export function Order() {
           calculateTotalPrice(orderItem.orderProduct) - 2000 * useCouponCount, //상품 가격
         buyer_email: account.email, //고객 이메일,
         buyer_nickName: account.nickName, // 고객 닉네임
-        buyer_tel: "010-1234-5678" /*구매자 연락처*/,
       },
       function (rsp) {
         const merchantUid = rsp.merchant_uid;
@@ -153,171 +161,267 @@ export function Order() {
   }
 
   return (
-    <Box maxWidth="1000px" mx={"auto"}>
-      <Box>
-        <Heading>Order</Heading>
-      </Box>
-      <Divider border={"1px solid black"} my={4} />
-      <Box bg={"black"} color={"white"} h={"40px"}>
-        이대역사거리점
-      </Box>
-      <Spacer />
-      <Accordion allowMultiple>
-        <AccordionItem>
-          <h2>
-            <AccordionButton>
-              <Flex gap={10}>
-                <Box fontWeight={"bold"} fontSiz={"xl"}>주문 상품</Box>
-                <Box as={"sub"}>{orderItem.orderProduct[0].productName}</Box>
-              </Flex>
-              <AccordionIcon />
-            </AccordionButton>
-          </h2>
-          <AccordionPanel pb={4}>
-            <Card>
-              <Stack divider={<StackDivider />} spacing="4">
-                {orderItem.orderProduct.map((product, index) => (
-                  <Flex key={index}>
-                    <Box>
-                      <Image
-                        src={
-                          "https://huistudybucket01.s3.ap-northeast-2.amazonaws.com/" +
-                          product.filePath
-                        }
-                        w={"50px"}
-                        h={"50px"}
-                      />
-                    </Box>
-                    <Box>
-                      <Box>{product.productName}</Box>
-                      {product.optionList.map((item, index) => (
-                        <Box key={index}>{item}</Box>
-                      ))}
-                      <Box>{product.count}개</Box>
-                    </Box>
-                    <Box>
-                      <Box fontWeight={"bold"}>
-                        {(product.totalPrice * product.count).toLocaleString(
-                          "ko-KR",
-                        )}
-                        원
-                      </Box>
-                    </Box>
-                  </Flex>
-                ))}
-              </Stack>
-            </Card>
-          </AccordionPanel>
-        </AccordionItem>
-      </Accordion>
-      <Box ml={3}>
-        <FormControl>
-          <FormLabel fontWeight={"bold"} color={"#777475"} mt={3}>매장 요청사항</FormLabel>
-          <Input
-            border={"1px solid #5b5859"}
-            placeholder={"매장 요청사항이 있으면 적어주세요."}
-            onChange={(e) => setRequest(e.target.value)}
-          />
-        </FormControl>
-        <RadioGroup onChange={setIsTakeOut} value={isTakeOut} mt={7} mb={7}>
-          <Box>포장 요청사항</Box>
-          <Radio size="md" value={"1"}  colorScheme='black'>
-            포장해주세요
-          </Radio>
-          <Radio size="md" value={"2"}  colorScheme='black'>
-            매장에서 먹고 갈게요
-          </Radio>
-        </RadioGroup>
-        {isTakeOut === "1" && (
-          <CheckboxGroup>
-            <Box>포장옵션</Box>
-            <Checkbox
-              size="md"
-              {...checkBoxStyle}
-              isChecked={option[0]}
-              onChange={(e) => setOption([e.target.checked, option[1]])}
-            >
-              캐리어/봉투 필요해요
-            </Checkbox>
-            <Checkbox
-              size="md"
-              {...checkBoxStyle}
-              isChecked={option[1]}
-              onChange={(e) => setOption([option[0], e.target.checked])}
-            >
-              빨대/스틱 필요해요
-            </Checkbox>
-          </CheckboxGroup>
-        )}
-        <Accordion allowMultiple mt={7}>
-          <AccordionItem>
-            <h2>
-              <AccordionButton>
-                <Box>
-                  <Box>쿠폰 적용</Box>
-                  <Flex>
-                    <Spacer />
-                    <Box>{couponCount}개 보유</Box>
-                    <ChevronRightIcon />
-                  </Flex>
-                </Box>
-              </AccordionButton>
-              <AccordionPanel>
-                <Box>쿠폰</Box>
-                <Flex>
-                  <Button onClick={reduceCouponCount}>-</Button>
-                  <Box>{useCouponCount}</Box>
-                  <Button onClick={plusCouponCount}>+</Button>
-                  <Box>개 사용</Box>
-                </Flex>
-              </AccordionPanel>
-            </h2>
-          </AccordionItem>
-        </Accordion>
-        <RadioGroup onChange={setProvider} value={provider} mt={7}>
-          <Box>결제 수단</Box>
-          <Radio size="md" value="html5_inicis" defaultChecked>
-            신용카드
-          </Radio>
-          <Radio size="md" value={"kakaopay"}>
-            카카오페이
-          </Radio>
-          <Radio size="md" value={"payco"}>
-            페이코
-          </Radio>
-          <Radio size="md" value={"uplus"}>
-            토스페이
-          </Radio>
-        </RadioGroup>
+    <Box>
+      <Box
+        height={"280px"}
+        backgroundColor={"#444444"}
+        textAlign={"center"}
+        display={"flex"}
+        alignItems={"center"}
+        justifyContent="center"
+      >
         <Box>
-          <Flex>
-            <Box>상품금액</Box>
-            <Spacer />
-            <Box>
-              {calculateTotalPrice(orderItem.orderProduct).toLocaleString(
-                "ko-KR",
-              )}
-              원
-            </Box>
-          </Flex>
-          <Flex>
-            <Box>할인금액</Box>
-            <Spacer />
-            <Box>-{useCouponCount * 2000}원</Box>
-          </Flex>
-          <Flex>
-            <Box>결제금액</Box>
-            <Spacer />
-            <Box>
-              {(
-                calculateTotalPrice(orderItem.orderProduct) -
-                useCouponCount * 2000
-              ).toLocaleString("ko-KR")}
-              원
-            </Box>
-          </Flex>
+          <Heading size="2xl" textColor={"#FDD000"}>
+            ORDER LIST
+          </Heading>
+          <Text fontSize={"lg"} textColor={"pink"}>
+            {orderItem.branchName}
+          </Text>
         </Box>
-        <Button onClick={handlePayment} colorScheme={"orange"}>결제하기</Button>
+      </Box>
+      <Box maxWidth="1200px" mx={"auto"} mt={"50px"}>
+        <Flex>
+          <Box w={"60%"}>
+            <Accordion allowMultiple>
+              <AccordionItem>
+                <h2>
+                  <AccordionButton>
+                    <Flex gap={10} alignItems={"center"} w={"100%"}>
+                      <Box fontWeight={"bold"} fontSize={"19px"} ml={3}>
+                        주문 상품
+                      </Box>
+                      <Spacer />
+                      <Box mr={2}>{orderItem.orderProduct[0].productName}</Box>
+                    </Flex>
+                    <AccordionIcon />
+                  </AccordionButton>
+                </h2>
+                <AccordionPanel>
+                  <Card variant={"outline"}>
+                    <Stack divider={<StackDivider />} spacing="4">
+                      {orderItem.orderProduct.map((product, index) => (
+                        <Flex key={index} alignItems={"center"} p={3}>
+                          <Box>
+                            <Image
+                              src={
+                                "https://huistudybucket01.s3.ap-northeast-2.amazonaws.com/" +
+                                product.filePath
+                              }
+                              h={"120px"}
+                            />
+                          </Box>
+                          <Box ml={4}>
+                            <Box fontSize={"18px"}>{product.productName}</Box>
+                            {product.optionList.map((item, index) => (
+                              <Box key={index} fontSize={"sm"}>
+                                {item}
+                              </Box>
+                            ))}
+                            <Box mt={3} fontSize={"21px"} mb={1}>
+                              {product.count}개
+                            </Box>
+                          </Box>
+                          <Spacer />
+                          <Box
+                            fontWeight={"bold"}
+                            fontSize={"23px"}
+                            mt={9}
+                            pr={3}
+                          >
+                            {(
+                              product.totalPrice * product.count
+                            ).toLocaleString("ko-KR")}
+                            원
+                          </Box>
+                        </Flex>
+                      ))}
+                    </Stack>
+                  </Card>
+                </AccordionPanel>
+              </AccordionItem>
+            </Accordion>
+            <Box ml={"30px"}>
+              <FormControl mt={"30px"}>
+                <FormLabel fontWeight={"bold"}>매장 요청사항</FormLabel>
+                <Input
+                  mt={"10px"}
+                  width={"90%"}
+                  border={"1px solid #5b5859"}
+                  placeholder={"매장 요청사항이 있으면 적어주세요."}
+                  fontSize={"13px"}
+                  onChange={(e) => setRequest(e.target.value)}
+                />
+              </FormControl>
+              <Box mt={"30px"}>
+                <Box fontWeight={"bold"}>포장 요청사항</Box>
+                <RadioGroup
+                  onChange={setIsTakeOut}
+                  value={isTakeOut}
+                  mt={"10px"}
+                >
+                  <Radio size="md" value={"1"} colorScheme="black" ml={2}>
+                    포장해주세요
+                  </Radio>
+                  <Radio size="md" value={"2"} colorScheme="black" ml={6}>
+                    매장에서 먹고 갈게요
+                  </Radio>
+                </RadioGroup>
+              </Box>
+              <Box>
+                <Box fontWeight={"bold"} mt={"30px"}>
+                  포장옵션
+                </Box>
+                <Box mt={"10px"}>
+                  {isTakeOut === "1" && (
+                    <CheckboxGroup>
+                      <Checkbox
+                        size="md"
+                        {...checkBoxStyle}
+                        isChecked={option[0]}
+                        onChange={(e) =>
+                          setOption([e.target.checked, option[1]])
+                        }
+                      >
+                        캐리어/봉투 필요해요
+                      </Checkbox>
+                      <Checkbox
+                        ml={6}
+                        size="md"
+                        {...checkBoxStyle}
+                        isChecked={option[1]}
+                        onChange={(e) =>
+                          setOption([option[0], e.target.checked])
+                        }
+                      >
+                        빨대/스틱 필요해요
+                      </Checkbox>
+                    </CheckboxGroup>
+                  )}
+                </Box>
+              </Box>
+            </Box>
+            <Box mt={"30px"}>
+              <Accordion allowMultiple>
+                <AccordionItem>
+                  <h2>
+                    <AccordionButton>
+                      <Flex w={"100%"} alignItems={"center"}>
+                        <Box fontWeight={"bold"} ml={3}>
+                          쿠폰 적용
+                        </Box>
+                        <Spacer />
+                        <Box fontSize={"sm"}>{couponCount}개 보유</Box>
+                        <ChevronRightIcon w={"20px"} />
+                      </Flex>
+                    </AccordionButton>
+                    <AccordionPanel>
+                      <Flex mt={3} alignItems={"center"}>
+                        <Box ml={"30px"} mr={5}>
+                          쿠폰
+                        </Box>
+                        <Button
+                          onClick={reduceCouponCount}
+                          {...RoundBlackButtonStyle}
+                        >
+                          -
+                        </Button>
+                        <Box mx={4} textAlign="center" fontSize={"lg"}>
+                          {useCouponCount}
+                        </Box>
+                        <Button
+                          {...RoundBlackButtonStyle}
+                          onClick={plusCouponCount}
+                        >
+                          +
+                        </Button>
+                        <Box ml={3}>개 사용</Box>
+                      </Flex>
+                    </AccordionPanel>
+                  </h2>
+                </AccordionItem>
+              </Accordion>
+            </Box>
+            <Box ml={"30px"} mb={"50px"}>
+              <Box fontWeight={"bold"} mt={"30px"}>
+                결제 수단
+              </Box>
+              <RadioGroup onChange={setProvider} value={provider} mt={"10px"}>
+                <Radio size="md" value="html5_inicis" defaultChecked ml={5}>
+                  신용카드
+                </Radio>
+                <Radio ml={3} size="md" value={"kakaopay"}>
+                  카카오페이
+                </Radio>
+                <Radio ml={3} size="md" value={"payco"}>
+                  페이코
+                </Radio>
+                <Radio ml={3} size="md" value={"uplus"}>
+                  토스페이
+                </Radio>
+              </RadioGroup>
+            </Box>
+          </Box>
+          <Box w={"40%"}>
+            <Box ml={"30px"} mr={"30px"} mt={"60px"}>
+              <Flex align={"center"}>
+                <Box fontSize={"lg"} as={"b"}>
+                  상품금액
+                </Box>
+                <Spacer />
+                <Box fontSize={"23px"} fontWeight={"bold"}>
+                  {calculateTotalPrice(orderItem.orderProduct).toLocaleString(
+                    "ko-KR",
+                  )}
+                  원
+                </Box>
+              </Flex>
+              <Flex align={"center"}>
+                <Box fontSize={"lg"} as={"b"}>
+                  할인금액
+                </Box>
+                <Spacer />
+                <Box fontSize={"23px"} fontWeight={"bold"}>
+                  -{useCouponCount * 2000}원
+                </Box>
+              </Flex>
+              <Flex align={"center"}>
+                <Box fontSize={"lg"} as={"b"}>
+                  결제금액
+                </Box>
+                <Spacer />
+                <Box fontSize={"25px"} color={"red"} fontWeight={"bold"}>
+                  {(
+                    calculateTotalPrice(orderItem.orderProduct) -
+                    useCouponCount * 2000
+                  ).toLocaleString("ko-KR")}
+                  원
+                </Box>
+              </Flex>
+            </Box>
+            <Box mt={"50px"}>
+              <Center ml={3}>
+                <ButtonGroup gap={4}>
+                  <Button
+                    width={"200px"}
+                    onClick={handlePayment}
+                    colorScheme={"orange"}
+                  >
+                    결제하기
+                  </Button>
+                  <Button
+                    width={"200px"}
+                    colorScheme={"pink"}
+                    onClick={() => {
+                      navigate(-1);
+                    }}
+                  >
+                    취소하기
+                  </Button>
+                </ButtonGroup>
+              </Center>
+            </Box>
+          </Box>
+        </Flex>
       </Box>
     </Box>
   );
