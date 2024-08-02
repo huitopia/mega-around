ea50bc0d935357fb94aabf586f8f6206ab2b3ee4
jnn-jnn1 <anj72005@gmail.com>
2024-08-03 04:07:56 +0900

    react : css 수정

--- frontend/src/event/Coupon.jsx
+++ frontend/src/event/Coupon.jsx
@@ -1,5 +1,5 @@
 import { Box, Center, Divider, Flex, Heading, Text } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { CouponComp } from "./component/CouponComp.jsx";
 
@@ -22,50 +22,58 @@ export function Coupon() {
   }
 
   return (
-    <Box maxWidth="1000px" mx={"auto"}>
-      <Box mt={"50px"} mb={"50px"}>
-        <Center>
-          <Heading>Coupon</Heading>
-        </Center>
+    <Box>
+      <Box
+        height={"280px"}
+        backgroundColor={"#444444"}
+        textAlign={"center"}
+        display={"flex"}
+        alignItems={"center"}
+        justifyContent="center"
+      >
+        <Box>
+          <Heading size="2xl" textColor={"#FDD000"}>
+            COUPON
+          </Heading>
+        </Box>
       </Box>
-      <Divider border={"1px solid black"} my={4} />
-      {loading ? (
-        <Box>Loading...</Box>
-      ) : couponCount > 0 ? (
-        <Box ml={3}>
-          <Flex
-            fontWeight={"bold"}
-            fontSize={"xl"}
-            alignItems="center"
-            mb={3}
-            gap={1}
-          >
-            <Text fontSize={"27px"}>사용할 수 있는 쿠폰이</Text>
-            <Text fontSize={"30px"} color={"red"}>
-              {couponCount}
-            </Text>
-            <Text fontSize={"27px"}>장 있어요!</Text>
-          </Flex>
-          <CouponComp />
-        </Box>
-      ) : (
-        <Center>
-          <Box fontSize={"18px"}>보유한 쿠폰이 없습니다.</Box>
-        </Center>
-      )}{" "}
-      <Box>
-        <Box fontWeight={"bold"} mt={"60px"} fontSize={"lg"} mb={3} ml={3}>
-          적립 및 사용내역
-        </Box>
-        {couponList.map((coupon, index) => (
-          <Box key={index} ml={5}>
-            <Box>{coupon.content}</Box>
-            <Box fontSize={"sm"}>{coupon.createdAtString}</Box>
-            {index < couponList.length - 1 && (
-              <Divider borderColor="gray.200" my={4} />
-            )}
+      <Box maxWidth="1000px" mx={"auto"} mt={10}>
+        {couponCount > 0 ? (
+          <Box ml={3}>
+            <Flex
+              fontWeight={"bold"}
+              fontSize={"xl"}
+              alignItems="center"
+              mb={3}
+              gap={1}
+            >
+              <Text fontSize={"27px"}>사용할 수 있는 쿠폰이</Text>
+              <Text fontSize={"30px"} color={"#FDD000"}>
+                {couponCount}
+              </Text>
+              <Text fontSize={"27px"}>장 있어요!</Text>
+            </Flex>
+            <CouponComp />
           </Box>
-        ))}
+        ) : (
+          <Center>
+            <Box fontSize={"18px"}>보유한 쿠폰이 없습니다.</Box>
+          </Center>
+        )}{" "}
+        <Box>
+          <Box fontWeight={"bold"} mt={"60px"} fontSize={"lg"} mb={3} ml={3}>
+            적립 및 사용내역
+          </Box>
+          {couponList.map((coupon, index) => (
+            <Box key={index} ml={5}>
+              <Box>{coupon.content}</Box>
+              <Box fontSize={"sm"}>{coupon.createdAtString}</Box>
+              {index < couponList.length - 1 && (
+                <Divider borderColor="gray.200" my={4} />
+              )}
+            </Box>
+          ))}
+        </Box>
       </Box>
     </Box>
   );
--- frontend/src/event/Stamp.jsx
+++ frontend/src/event/Stamp.jsx
@@ -49,89 +49,120 @@ function Stamp(props) {
   }
 
   return (
-    <Box maxWidth="1000px" mx={"auto"}>
-      <Flex justify={"right"} mt={"50px"} alignItems={"center"}>
-        <Flex gap={2}>
-          <Heading>스탬프</Heading>
-          <Heading color={"red"}>{stampCount}</Heading>
+    <Box>
+      <Box
+        height={"280px"}
+        backgroundColor={"#444444"}
+        textAlign={"center"}
+        display={"flex"}
+        alignItems={"center"}
+        justifyContent="center"
+      >
+        <Box>
+          <Heading size="2xl" textColor={"#FDD000"}>
+            STAMP
+          </Heading>
+          <Text fontSize={"lg"} textColor={"pink"}>
+            스탬프 조회
+          </Text>
+        </Box>
+      </Box>
+      <Box maxWidth="1000px" mx={"auto"}>
+        <Flex justify={"right"} mt={"50px"} alignItems={"center"}>
+          <Flex gap={2}>
+            <Heading>스탬프</Heading>
+            <Heading color={"#FDD000"}>{stampCount}</Heading>
+          </Flex>
+          <Spacer />
+          <Button
+            onClick={() => onOpen()}
+            colorScheme={"orange"}
+            borderRadius={"20px"}
+            w={"170px"}
+          >
+            적립내역 보기
+          </Button>
         </Flex>
-        <Spacer />
-        <Button
-          onClick={() => onOpen()}
-          colorScheme={"orange"}
-          borderRadius={"20px"}
-          w={"170px"}
-        >
-          적립내역 보기
-        </Button>
-      </Flex>
-      <Flex justify={"center"} mt={10}>
-        <Box bgColor={"yellow"} w={"520px"} h={"300px"} borderRadius={"20px"}>
-          <Flex alignItems="center" height="300" justifyContent="center">
-            <Grid templateColumns="repeat(5, 1fr)" gap={7} alignItems="center">
-              {boxes.map((box) => (
-                <Box key={box}>
-                  {stampCount < box ? (
-                    <Box
-                      bg="rgba(0, 0, 0, 0.1)"
-                      borderRadius="full"
-                      w="50px"
-                      h="50px"
-                      display="flex"
-                      alignItems="center"
-                      justifyContent="center"
-                      color="white"
-                    >
-                      {box}
-                    </Box>
-                  ) : (
-                    <Box
-                      position="relative"
-                      display="inline-block"
-                      cursor="pointer"
-                      _hover={{ ".description": { opacity: 1 } }}
-                    >
-                      <Image
-                        w="50px"
-                        h="80px"
-                        src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbXtHba%2FbtsICfOOFVO%2Fg6xk09ztnHMPKMJQKEuYjK%2Fimg.png"
-                      />
+        <Flex justify={"center"} mt={10}>
+          <Box
+            bgColor={"#FDD000"}
+            w={"520px"}
+            h={"300px"}
+            borderRadius={"20px"}
+          >
+            <Flex alignItems="center" height="300" justifyContent="center">
+              <Grid
+                templateColumns="repeat(5, 1fr)"
+                gap={7}
+                alignItems="center"
+              >
+                {boxes.map((box) => (
+                  <Box key={box}>
+                    {stampCount < box ? (
                       <Box
-                        w={"150px"}
-                        h={"40px"}
-                        className="description"
-                        position="absolute"
-                        top="12"
-                        left="-10"
-                        right="0"
-                        bottom="0"
-                        bg="rgba(0, 0, 0, 0.5)"
-                        color="white"
+                        bg="rgba(0, 0, 0, 0.1)"
+                        borderRadius="full"
+                        w="50px"
+                        h="50px"
                         display="flex"
                         alignItems="center"
                         justifyContent="center"
-                        opacity="0"
-                        transition="opacity 0.3s ease"
-                        zIndex={1}
-                        borderRadius={"10px"}
+                        color="white"
                       >
-                        <Text fontSize="xs"> 발급일: {stampDate[box - 1]}</Text>
+                        {box}
                       </Box>
-                    </Box>
-                  )}
-                </Box>
-              ))}
-            </Grid>
-          </Flex>
+                    ) : (
+                      <Box
+                        position="relative"
+                        display="inline-block"
+                        cursor="pointer"
+                        _hover={{ ".description": { opacity: 1 } }}
+                      >
+                        <Image
+                          w="50px"
+                          h="80px"
+                          src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbXtHba%2FbtsICfOOFVO%2Fg6xk09ztnHMPKMJQKEuYjK%2Fimg.png"
+                        />
+                        <Box
+                          w={"150px"}
+                          h={"40px"}
+                          className="description"
+                          position="absolute"
+                          top="12"
+                          left="-10"
+                          right="0"
+                          bottom="0"
+                          bg="rgba(0, 0, 0, 0.5)"
+                          color="white"
+                          display="flex"
+                          alignItems="center"
+                          justifyContent="center"
+                          opacity="0"
+                          transition="opacity 0.3s ease"
+                          zIndex={1}
+                          borderRadius={"10px"}
+                        >
+                          <Text fontSize="xs">
+                            {" "}
+                            발급일: {stampDate[box - 1]}
+                          </Text>
+                        </Box>
+                      </Box>
+                    )}
+                  </Box>
+                ))}
+              </Grid>
+            </Flex>
+          </Box>
+        </Flex>
+        <Box bg={"#f8f9fa"} mt={12} p={7}>
+          <Text fontWeight={"bold"}>유의사항</Text>
+          <Box whiteSpace="pre-line" fontSize={"sm"} lineHeight={2}>
+            {longText}
+          </Box>
         </Box>
-      </Flex>
-      <Box bg={"#f8f9fa"} mt={12} p={7}>
-        <Text fontWeight={"bold"}>유의사항</Text>
-        <Box whiteSpace="pre-line" fontSize={"sm"} lineHeight={2}>
-          {longText}
-        </Box>
+        <StampModal isOpen={isOpen} onClose={onClose} />
       </Box>
-      <StampModal isOpen={isOpen} onClose={onClose} />
     </Box>
   );
 }
