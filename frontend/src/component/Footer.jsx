08f0320f2ca0b85125d00ef72638941f982ac80e
huitopia <huitopia27@gmail.com>
2024-08-02 11:39:50 +0900

    react : css 수정

--- frontend/src/component/Footer.jsx
+++ frontend/src/component/Footer.jsx
@@ -1,8 +1,8 @@
 import { Box } from "@chakra-ui/react";
 
 export const Footer = () => (
-  <Box backgroundColor={"yellow"} height={"200px"} mt={"100px"} width={"100%"}>
+  <Box height={"200px"} mt={"100px"} width={"100%"}>
     <Box height={"50px"} backgroundColor={"#444444"}></Box>
-    <Box height={"150px"}></Box>
+    <Box height={"150px"} backgroundColor={"yellow"}></Box>
   </Box>
 );
