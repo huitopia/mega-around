cac751f5e83655d1eb24a5df1de6b24cc5a638c3
huitopia <huitopia27@gmail.com>
2024-08-06 00:59:49 +0900

    spring : ResponseEntity 수정

--- backend/src/main/java/com/backend/controller/branch/BranchController.java
+++ backend/src/main/java/com/backend/controller/branch/BranchController.java
@@ -3,6 +3,8 @@ package com.backend.controller.branch;
 import com.backend.domain.user.BranchGeocode;
 import com.backend.service.branch.BranchService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Description;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,8 +20,12 @@ public class BranchController {
     private final BranchService service;
 
     @GetMapping("/location")
+    @Description("가까운 지점 조회")
     public ResponseEntity getUserAroundBranch(@RequestParam double lat, @RequestParam double lng) {
         List<BranchGeocode> branchGeocodes = service.selectAroundBranchList(lat, lng);
-        return ResponseEntity.ok().body(branchGeocodes);
+        if (branchGeocodes.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(branchGeocodes);
     }
 }
